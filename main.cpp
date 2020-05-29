bb57f1ad985a3285816ead4cb7c8944a0317b9d2
campital <carbaxton@protonmail.com>
2020-05-29 18:25:21 -0400

    Added command line opts and fixed memory leak

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include <cstring>
 #include <openssl/err.h>
 #include <unistd.h>
+#include <getopt.h>
+
+// for memory freeing later
+SSL_CTX *ctx;
 
 void closeSocket(socket_pair pair)
 {
     if(pair.sockSSL != nullptr) {
         SSL_shutdown(pair.sockSSL);
         SSL_free(pair.sockSSL);
+        SSL_CTX_free(ctx);
     }
     if(pair.basicSock != -1) {
         close(pair.basicSock);
@@ -16,7 +21,7 @@ void closeSocket(socket_pair pair)
 
 SSL* secureSocket(int normalSock)
 {
-    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    ctx = SSL_CTX_new(TLS_client_method());
     SSL* result = SSL_new(ctx);
     if(result == NULL) {
         std::cerr << "Could not create SSL object!\n";
@@ -67,8 +72,51 @@ socket_pair sslConnect(std::string hostName)
     return pair;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    // get the cmd line arguments
+    // only runs getopt_long once since multiple args cannot be used simultaneously right now
+    option validLongs[] = {
+        {"help", 0, NULL, (int)'h'},
+        {"gui", 0, NULL, (int)'g'},
+        {"simple", 0, NULL, (int)'s'},
+        {"fancy", 0, NULL, (int)'f'},
+        {}
+    };
+    char option = (char)getopt_long(argc, argv, "hgsf", validLongs, NULL);
+    const std::string argErrorString = "Try '" + std::string(argv[0]) + " --help' for more information.\n";
+    // TODO: Implement mode switching when an option is chosen to change how the content is displayed
+    switch(option) {
+        case -1:
+            // TODO: set default option
+            break;
+        case '?':
+            std::cerr << argErrorString;
+            return 0;
+            break;
+        case 'h':
+            std::cerr << "Usage: " << argv[0] << " [options]\nClibean is a desktop client for Membean (https://membean.com/)\n\n" 
+            << "  -h, --help      Display this help menu\n"
+            << "  -g, --gui       Run Clibean in GUI mode\n"
+            << "  -s, --simple    Run Clibean in simple terminal mode (default)\n"
+            << "  -f, --fancy     Run Clibean in fancy terminal mode (ncurses)\n";
+            return 0;
+            break;
+        case 'g':
+            // TODO
+            break;
+         case 's':
+            // TODO
+            break;
+         case 'f':
+            // TODO
+            break;
+        default:
+            std::cerr << argv[0] << ": Invalid usage!\n" << argErrorString;
+            return 0;
+            break;
+    }
+
     socket_pair mbConnection = sslConnect("membean.com");
     closeSocket(mbConnection);
     return 0;
